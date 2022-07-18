16cedfcd5a47bf55ea9f35e7c079ecc4fc8d8774
rosec <roseckasara@gmail.com>
2022-07-18 11:29:38 +0200

    20220718 - provolána api na zjištění kurzu

--- src/main/java/BTCTrader.java
+++ src/main/java/BTCTrader.java
@@ -1,6 +1,12 @@
+import TradingInfo.BTCValue;
+
+import java.io.IOException;
+
 public class BTCTrader {
-        public static void main (String[] arg){
+        public static void main (String[] arg) throws IOException {
             System.out.println("Hello BTC");
+            BTCValue trade = new BTCValue();
+            trade.connect();
         }
 
 }
--- src/main/java/TradingInfo/BTCValue.java
+++ src/main/java/TradingInfo/BTCValue.java
@@ -1,32 +1,86 @@
 package TradingInfo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import netscape.javascript.JSException;
+import netscape.javascript.JSObject;
+
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
 import java.net.URLConnection;
 
 public class BTCValue {
+    static JSObject BTCexchangeRate = new JSObject() {
+        @Override
+        public Object call(String methodName, Object... args) throws JSException {
+            return null;
+        }
 
+        @Override
+        public Object eval(String s) throws JSException {
+            return null;
+        }
 
-    URL url = new URL("https://blockchain.info/ticker");//vrátí json
+        @Override
+        public Object getMember(String name) throws JSException {
+            return null;
+        }
 
-   // URLConnection con = url.openConnection();
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestMethod("GET");
+        @Override
+        public void setMember(String name, Object value) throws JSException {
+
+        }
+
+        @Override
+        public void removeMember(String name) throws JSException {
+
+        }
+
+        @Override
+        public Object getSlot(int index) throws JSException {
+            return null;
+        }
+
+        @Override
+        public void setSlot(int index, Object value) throws JSException {
+
+        }
+    };
+
+    public static boolean connect() throws IOException {
+       // URL url = new URL("https://blockchain.info/tobtc?currency=EUR&value=500");//vrátí jednu hodnotu
+        URL url = new URL("https://blockchain.info/ticker");//vrátí json
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36");
+        int responseCode = con.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            BTCexchangeRate = response;
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+
+        con.disconnect();
+        return true;
+    }
+
 
     public BTCValue() throws IOException {
 
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-            String line = null;
-
-            // read each line and write to System.out
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        }
     }
 }
+//popsat co se kde děje
